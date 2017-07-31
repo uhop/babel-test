8396de7d89392678a68a9eb31845a878f91c086e
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2017-07-30 21:41:56 -0500

    Switch to dcl6 wrapping.

--- src/hello-world.js
+++ src/hello-world.js
@@ -1,6 +1,15 @@
-class HelloWorld extends HTMLElement {
+// plain version
+// class HelloWorld extends HTMLElement {
+//   connectedCallback () {
+//     this.appendChild(document.createTextNode('Hello, world!'));
+//   }
+// }
+
+// dcl6 version
+const HelloWorld = dcl(HTMLElement, Base => class extends Base {
   connectedCallback () {
     this.appendChild(document.createTextNode('Hello, world!'));
   }
-}
+});
+
 customElements.define('hello-world', HelloWorld);
